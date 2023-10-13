9ac117878fdf441048f34e3451eb23e34bbb7255
FIXED-TERM Doan Tran Ba Dat (MS/ETA7) <ddn4hc@bosch.com>
2023-10-13 16:34:30 +0700

    fix ui

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -34,7 +34,7 @@ const UserInfo = (props: IProps) => {
     
     return (
         <div className={`absolute sm:left-[-150px] rounded-[8px] left-[-150px] top-[100%] bg-white border  shadow-lg `}>
-            <div className="hover:bg-slate-400 px-2 pt-2 py-1 flex items-center border-black"><AiOutlineUser className="text-xl"/> <p className="w-[120px] overflow-hidden text-ellipsis mx-2 text-sm">{user?.fullName}</p></div>
+            <div className="hover:bg-slate-400 px-2 pt-2 py-1 flex items-center border-black"><AiOutlineUser className="text-xl"/> <p className="w-[120px] truncate mx-2 text-sm">{user?.fullName}</p></div>
             <div onClick={onLogout} className="hover:bg-slate-400 px-2 pt-2 py-1  flex items-center"><RiLogoutBoxLine className="text-xl"/> <p className="mx-2 text-sm">Log out</p></div>
         </div>
     )
