8eb0ca3ed301bb44af3328624c78d401690f30a0
FIXED-TERM Doan Tran Ba Dat (MS/ETA4) <ddn4hc@bosch.com>
2024-02-21 11:30:42 +0700

    fix add type genai

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,7 +14,7 @@ const Login = () => {
     const dispatch = useDispatch();
     const [isLoading, setIsLoading] = useState(false);
     const [showBtnSave, setShowBtnSave] = useState(false);
-    const [validEmail, setValidEmail] = useState(false);
+    const [validEmail, setValidEmail] = useState(true);
 
     useEffect(() => {
         setShowBtnSave(!!(email && password));
